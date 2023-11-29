ea6c39965066294cb95287c7348f01a15a1a333e
Zac Warner <zektgn@gmail.com>
2023-11-29 12:15:16 -0800

    fixed theme

--- customCompareReact/src/main.tsx
+++ customCompareReact/src/main.tsx
@@ -4,11 +4,11 @@ import App from './App'
 import Navbar from './components/navbar'
 import './index.css'
 import { ThemeProvider } from '@mui/material/styles';
-import psomasTheme from './theme';
+import theme from './theme';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ThemeProvider theme={psomasTheme}>
+  <ThemeProvider theme={theme}>
     <React.StrictMode>
       <Navbar />
       <App />
